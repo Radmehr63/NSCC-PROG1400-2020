9eacdd58b514de59797fc9ed9dc713b6072e9580
HBS-Hydrostone <w0432329@nscc.ca>
2020-01-09 12:18:00 -0400

    helloWorld changed to display number

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -3,7 +3,11 @@
 
         public static void main(String[] args) {
 
-            System.out.println("Hello World");
+            int num;
+
+            num = 123456;
+
+            System.out.println("num = " + num);
         }
 
     }
