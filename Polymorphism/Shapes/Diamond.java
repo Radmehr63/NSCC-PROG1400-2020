d2ac789aa627a2dc8b3f880e80fcb4bb609abc27
HBS-Hydrostone <w0432329@nscc.ca>
2020-03-12 12:10:43 -0300

    Polymorphism until the end of March 12, 2020

---
+++ Polymorphism/Shapes/Diamond.java
@@ -0,0 +1,42 @@
+package Polymorphism.Shapes;
+
+public class Diamond extends Shape {
+
+    public Diamond() {
+        lines.add("         *         ");
+        lines.add("       *   *       ");
+        lines.add("     *       *     ");
+        lines.add("   *           *   ");
+        lines.add(" *               * ");
+        lines.add("   *           *   ");
+        lines.add("     *       *     ");
+        lines.add("       *   *       ");
+        lines.add("         *         ");
+    }
+
+    @Override
+    public void draw(){
+//        System.out.println("         * ");
+//        System.out.println("       *   * ");
+//        System.out.println("     *       * ");
+//        System.out.println("   *           * ");
+//        System.out.println(" *               * ");
+//        System.out.println("   *           * ");
+//        System.out.println("     *       * ");
+//        System.out.println("       *   * ");
+//        System.out.println("         * ");
+        for (String line: lines) {
+            System.out.println(line);
+        }
+    }
+
+    @Override
+    public void drawLine(int index) {
+        if (index >= lines.size()) {
+            System.out.print("                ");
+        }else {
+            System.out.print(lines.get(index));
+        }
+    }
+
+}
---
+++ out/production/NSCC-PROG1400-2020/README.md
@@ -0,0 +1,2 @@
+# NSCC-PROG1400-2020
+Intro to Object Oriented Programming
